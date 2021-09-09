96486fd41e64bcbaa7ca1698ce66e55c78008949
Muhammad Yahiya <64965102+Yahiya166@users.noreply.github.com>
2021-09-09 11:02:20 +0500

    first commit

--- main/quiz/main.js
+++ main/quiz/main.js
@@ -2,50 +2,85 @@ var ques = document.getElementById('ques');
 var opt = document.getElementById('opt');
 var currentQuestion = document.getElementById('currentQuestion');
 var totalQuestion = document.getElementById('totalQuestion');
+var disab = document.getElementById('next');
+var score = 0;
+var marks = 0;
+
 
 var arr = [{
     question: "HTML stands for  ____________",
     option: ['Hyper Text Mark Up Language', 'Haaperr Tittle Marks Up Language', 'Hypar Text Merk Up Languege', 'Hidrogen Text Mark Up Language'],
     correctoption: 'Hyper Text Mark Up Language'
 }, {
-    question: "HTML stands for ____________",
-    option: ['Hyper Text Mark Up Language', 'Haaperr Tittle Marks Up Language', 'Hypar Text Merk Up Languege', 'Hidrogen Text Mark Up Language'],
-    correctoption: 'Hyper Text Mark Up Language'
+    question: "How many headings in html ____________",
+    option: ['3', '5', '6'],
+    correctoption: '6'
 }, {
-    question: "HTML stands for  ____________",
-    option: ['Hyper Text Mark Up Language', 'Haaperr Tittle Marks Up Language', 'Hypar Text Merk Up Languege', 'Hidrogen Text Mark Up Language'],
-    correctoption: 'Hyper Text Mark Up Language'
+    question: "para is ___________",
+    option: ['inline', 'block', 'horizental', 'vertical'],
+    correctoption: 'block'
 }, {
-    question: "HTML stands for ____________",
-    option: ['Hyper Text Mark Up Language', 'Haaperr Tittle Marks Up Language', 'Hypar Text Merk Up Languege', 'Hidrogen Text Mark Up Language'],
-    correctoption: 'Hyper Text Mark Up Language'
+    question: "Which character is used to indicate an end tag?",
+    option: ['^', '/', '<', '*'],
+    correctoption: '/'
 }, {
-    question: "HTML stands for ____________",
-    option: ['Hyper Text Mark Up Language', 'Haaperr Tittle Marks Up Language', 'Hypar Text Merk Up Languege', 'Hidrogen Text Mark Up Language'],
-    correctoption: 'Hyper Text Mark Up Language'
-}, {
-    question: "HTML stands for ____________",
-    option: ['Hyper Text Mark Up Language', 'Haaperr Tittle Marks Up Language', 'Hypar Text Merk Up Languege', 'Hidrogen Text Mark Up Language'],
-    correctoption: 'Hyper Text Mark Up Language'
-}, {
-    question: "HTML stands for ____________",
-    option: ['Hyper Text Mark Up Language', 'Haaperr Tittle Marks Up Language', 'Hypar Text Merk Up Languege', 'Hidrogen Text Mark Up Language'],
-    correctoption: 'Hyper Text Mark Up Language'
-}, {
-    question: "HTML stands for ____________",
-    option: ['Hyper Text Mark Up Language', 'Haaperr Tittle Marks Up Language', 'Hypar Text Merk Up Languege', 'Hidrogen Text Mark Up Language'],
-    correctoption: 'Hyper Text Mark Up Language'
-}, {
-    question: "HTML stands for ____________",
-    option: ['Hyper Text Mark Up Language', 'Haaperr Tittle Marks Up Language', 'Hypar Text Merk Up Languege', 'Hidrogen Text Mark Up Language'],
-    correctoption: 'Hyper Text Mark Up Language'
-}, {
-    question: "HTML stands for ____________",
-    option: ['Hyper Text Mark Up Language', 'Haaperr Tittle Marks Up Language', 'Hypar Text Merk Up Languege', 'Hidrogen Text Mark Up Language'],
-    correctoption: 'Hyper Text Mark Up Language'
-}]
+    question: "Who is making the Web standards?",
+    option: ['Google', 'Microsoft', 'The World Wide Web Consortium', 'Mozila'],
+    correctoption: 'The World Wide Web Consortium'
+}
+// , {
+//     question: "Choose the correct HTML element for the largest heading:",
+//     option: ['<h1>', '<heading>', '<head>', '<h6>'],
+//     correctoption: '<h1>'
+// }, {
+//     question: "What is the correct HTML element for inserting a line break?",
+//     option: ['<br />', '<lb>', '<break>'],
+//     correctoption: '<br />'
+// }, {
+//     question: "Choose the correct HTML element to define important text",
+//     option: ['<i>', '<strong>', '<important>', '<b>'],
+//     correctoption: '<b>'
+// }, {
+//     question: "Choose the correct HTML element to define emphasized text",
+//     option: ['<italic>', '<em>', '<i>'],
+//     correctoption: '<em>'
+// }, {
+//     question: "What is the correct HTML for creating a hyperlink?",
+//     option: ["<a href='https://www.w3school.com'>W3Schools</a>", "<a name='https://www.w3school.com'>W3Schools</a>", "<a URL='https://www.w3school.com'>W3Schools</a>"],
+//     correctoption: "<a href='https://www.w3school.com'>W3Schools</a>"
+// }
+]
 
-var score = 0
+function cond() {
+    if(!(score == totalQuestion.length)){
+        score++
+        startQuiz()
+    }else{
+        alert('Quiz Finish')
+        // window.location.href('../../index.html')
+    }
+}
+
+function next() {
+    if (disab.disabled = 'true') {
+        disab.disabled = disab.enabled;
+    }
+   
+}
+
+
+
+
+function butt(optValue, answerValue) {
+    if (optValue == answerValue) {
+        marks++
+        console.log(marks)
+    }
+    next()
+}
+
+
+
 
 function startQuiz() {
     currentQuestion.innerHTML = score + 1;
@@ -55,60 +90,12 @@ function startQuiz() {
 
     for (var i = 0; i < arr[score].option.length; i++) {
         opt.innerHTML += `<div class="col-md-6 text-center my-2 ">
-     <button type="button" class="btn btn1 text-white darkBackground fs-4 py-3 mt-3" onclick="click(${arr[score].option[i]},${arr[score].correctoption})"> ${arr[score].option[i]} </button>
-        </div>`
+     <button type="button" class="btn btn1 text-white darkBackground fs-4 py-3 mt-3 w-75" onclick="butt()"> ${arr[score].option[i]} </button> </div>`
     }
 
+    disab.disabled = 'true'
 }
 
 startQuiz()
 
-// console.log(arr[0]);
-
-// var opt = document.getElementById('opt');
-
-// opt.innerHTML = ''
-
-// ques.innerText = arr[0].question;
-
-// opt.innerHTML += `<div class="col-md-6 text-center my-2 ">
-// <button type="button" class="btn btn1 text-white darkBackground fs-4 py-3 mt-3" onclick="click()"> ${arr[0].option[0]} </button>
-// </div>` 
-
-
-// function running() {
-
-//     var ques = document.getElementById('ques');
-//     var opt = document.getElementById('opt');
-//     for (var i = 0; i < arr.length; i++) {
-
-//         opt.innerHTML = '';
-//         // ques.innerHTML = '';
-
-//         // console.log(ques.innerHTML);
-//         ques.innerHTML = arr[i].question;
-//         console.log(ques.innerHTML);
-
-//         for (var j = 0; j )
-//             opt.innerHTML += `<div class="col-md-6 text-center my-2 ">
-// <button type="button" class="btn btn1 text-white darkBackground fs-4 py-3 mt-3" onclick="click()"> ${arr[i].option[0]} </button>
-// </div>`
-
-
-//         // var ques = document.getElementById('ques');
-
-//         // ques.innerText = arr[i].question;
-//     }
-// }
-
-
-// function click() {
-//     var disab = document.getElementById('next');
-
-//     if (disab.disabled == true) {
-//         disab.disabled = disab.enabled;
-//     }
-
-// }
-
-// running()
+// ${arr[score].option[i]},${arr[score].correctoption}
